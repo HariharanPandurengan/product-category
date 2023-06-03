20a6f882e7d6d348f438082fc43fa3ed27dbddb6
HariHaran Pandurengan <hariharanpandurengan@gmail.com>
2023-06-03 13:41:36 +0530

    pc

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -20,7 +20,7 @@ function Navbar() {
                     <li><Link className="links" to="Headphones">HeadPhones</Link></li>
                 </ul>
             </div>
-            <svg onClick={()=>setShow(true)} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+            <svg className="svg-path" onClick={()=>setShow(true)} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <path fill="currentColor" d="M3 6h18v2H3zm0 5h18v2H3zm0 5h18v2H3z"/>
             </svg>
         </div>
